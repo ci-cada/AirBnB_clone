1860ca630be3b19d0f58e5f36dc63ec1a1fb29e0
tesshasnochills <tess.nganga2020@gmail.com>
2023-10-10 10:53:19 +0300

    files

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""model - base model"""
+import uuid
+from datetime import datetime
+import models
+
+
+
+class BaseModel:
+    """base class for hbnb"""
+    def __init__(self, *args, **kwargs):
+        """initialization of bse model variables"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+
+
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], 
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+
+            
+            if "updated_at" in kwargs:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], 
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+
+    def __str__(self):
+        """str function to return class name id and dictionary"""
+        return "[{}] ({}) {}".format(self.__class__.__name,
+                                     self.id, 
+                                     self.__dict__)
+    
+    def to_dict(self):
+        "dict function to return the created dictionary"
+        dict_obj = self.__dict__.copy()
+        dict_obj["__class__"] = self.__class.__name__
+        dict_obj["created_at"] = self.created_at.isoformat()
+        dict_obj["updated_at"] = self.updated_at.isoformat()
+        return dict_obj
