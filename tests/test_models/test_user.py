9122d76e1f565049e5bb68bd3402e5e5a57f7c63
tesshasnochills <tess.nganga2020@gmail.com>
2023-10-13 13:29:24 +0300

    nothing

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
-"""model - user test"""
-
-from models.user import User
+"""model - user test case"""
 from tests.test_models.test_base_model import TestBaseModel
+from models.user import User
+
 
 class TestUser(TestBaseModel):
     """testing the user model"""
 
     def __init__(self, *args, **kwargs):
-        """initialization"""
+        """ the init function """
 
         super().__init__(*args, **kwargs)
         self.name = "User"
         self.value = User
 
     def test_first_name(self):
-        """testing if the first name is a string"""
-        User = self.value
+        """  testing if the first name is a string"""
+        User = self.value()
         self.assertEqual(type(User.first_name), str)
 
     def test_last_name(self):
-        """testing if the last name is a str"""
-        User = self.value
-        self.assertEqual(type(User.last_name), str)
-
-    def test_password(self):
-        """testing if the password is a str"""
-        User = self.value
-        self.assertEqual(type(User.password), str)
+        """testing whether the last name is a string"""
+        user = self.value()
+        self.assertEqual(type(user.last_name), str)
 
     def test_email(self):
-        User = self.value
-        self.assertEqual(type(User.email), str)
+        """testing if the email is a string"""
+        user = self.value()
+        self.assertEqual(type(user.email), str)
+
+    def test_password(self):
+        """testing if the password is a string """
+        user = self.value()
+        self.assertEqual(type(user.password), str)
