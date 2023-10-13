c910f241c57937174e874b775b70aaae08b5d923
ci-cada <ciphedc@gmail.com>
2023-10-13 12:48:44 +0300

    6 console 001

--- console.py
+++ console.py
@@ -1,2 +1,44 @@
+"""Method Command Interpreter"""
 import cmd
-from models import *
+import shlex
+import models
+from datetime import datetime
+from models.base_model import BaseModel
+from models import storage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+    __classes = [
+        "Amenity",
+        "BaseModel",
+        "City",
+        "Place",
+        "Review",
+        "State",
+        "User"
+    ]
+
+    def do_quit(self, args):
+        '''<Quit> Command To Exit The Program'''
+        return True
+
+    def do_EOF(self, args):
+        '''Handles end of file'''
+        return True
+
+    def emptyline(self):
+        '''dont execute anything when user
+           press enter an empty line
+        '''
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
